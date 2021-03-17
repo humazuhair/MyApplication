17a4959a6cb4730e1a2d4cc5f73adcc3c937e30e
huma <56148533+humazuhair@users.noreply.github.com>
2021-03-17 14:18:36 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,1 +1,168 @@
-package com.example.myapplication;import android.content.Context;import android.content.Intent;import android.content.SharedPreferences;import android.view.View;import android.widget.Button;import android.widget.EditText;import android.widget.Toast;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class MainActivity extends AppCompatActivity {    String valeur ="";    protected void onSaveInstanceState(Bundle savedInstanceState) {        super.onSaveInstanceState(savedInstanceState);        savedInstanceState.putString("clé", valeur);    }    public void onRestoreInstanceState(Bundle savedInstanceState) {        super.onRestoreInstanceState(savedInstanceState);        String valeur = savedInstanceState.getString("clé");    }    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);        Button btnQuitter = (Button) findViewById(R.id.btnQuitter);        btnQuitter.setOnClickListener(btnQuitterOnClickListener);        popUp("onCreate()");        Button btnEnvoyer = (Button) findViewById(R.id.btnEnvoyer);        btnEnvoyer.setOnClickListener(btnEnvoyerOnClickListener);        Button btnAct = (Button) findViewById(R.id.btnAct2);        btnAct.setOnClickListener(btnAct2);        if (savedInstanceState != null){            valeur = savedInstanceState.getString("clé");        }    }    public void setTxTValeur(String valeur) {        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);        zoneValeur.setText(valeur);    }    public String getTxtValeur() {        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);        return zoneValeur.getText().toString();    }    View.OnClickListener btnAct2 = new View.OnClickListener() {        @Override        public void onClick(View v) {            Intent intent = new Intent(v.getContext(), SecondActivity.class);            startActivity(intent);            intent.putExtra("clé", getTxtValeur());        }    };    View.OnClickListener btnEnvoyerOnClickListener = new View.OnClickListener() {        @Override        public void onClick(View v) {            popUp("valeur saisie = " + getTxtValeur());        }    };    /** =============================================================     * Exécuté que l'activité arrêtée via un "stop" redémarre.     *     * La fonction onRestart() est suivie de la fonction onStart().     */    @Override    protected void onRestart() {        super.onRestart();        popUp("onRestart()");    }    /** ==============================================================     * Exécuté lorsque l'activité devient visible à l'utilisateur.     *     * La fonction onStart() est suivie de la fonction onResume().     */    @Override    protected void onStart() {        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);        setTxTValeur(settings.getString("valeur", ""));        super.onStart();        popUp("onStart()");    }    /** ==============================================================     * Exécutée à chaque passage en premier plan de l'activité.     * Ou bien, si l'activité passe à nouveau en premier     * (si une autre activité était passée en premier plan entre temps).     *     * La fonction onResume() est suivie de l'exécution de l'activité.     */    @Override    protected void onResume() {        super.onResume();        popUp("onResume()");    }    /** =============================================================     * La fonction onPause() est suivie :     * - d'un onResume() si l'activité passe à nouveau en premier plan     * - d'un onStop() si elle devient invisible à l'utilisateur     *     * L'exécution de la fonction onPause() doit être rapide,     * car la prochaine activité ne démarrera pas tant que l'exécution     * de la fonction onPause() n'est pas terminée.     */    @Override    protected void onPause() {        super.onPause();        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);        SharedPreferences.Editor editor = settings.edit();        editor.putString("valeur", getTxtValeur());        editor.commit();        if (isFinishing()) {            popUp("onPause, l'utilisateur à demandé la fermeture via un finish()");        } else {            popUp("onPause, l'utilisateur n'a pas demandé la fermeture via un finish()");        }    }    /** ==============================================================     * La fonction onStop() est exécutée :     * - lorsque l'activité n'est plus en premier plan     * - ou bien lorsque l'activité va être détruite     *     * Cette fonction est suivie :     * - de la fonction onRestart() si l'activité passe à nouveau en premier plan     * - de la fonction onDestroy() lorsque l'activité se termine     *    ou bien lorsque le système décide de l'arrêter     */    @Override    protected void onStop() {        super.onStop();        popUp("onStop()");    }    /** =============================================================     * Cette fonction est exécutée lorsque l'activité se termine ou bien lorsque     * le système décide de l'arrêter.     *     * La fonction onCreate() devra à nouveau être exécutée pour obtenir à nouveau l'activité.     */    @Override    protected void onDestroy() {        super.onDestroy();        popUp("onDestroy()");    }    //=================================================================    View.OnClickListener btnQuitterOnClickListener = new View.OnClickListener() {        @Override        public void onClick(View v) {            finish();        }    };    public void popUp(String message) {        Toast.makeText(this, message, Toast.LENGTH_LONG).show();    }}
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String valeur ="";
+
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("clé", valeur);
+    }
+  
+  /* c) Le premier cas donne les extras de l'intention qui a démarré cette activité, tandis que le second est utilisé lorsque onCreate est invoqué la deuxième fois et plus, par exemple, lors de la rotation de l'appareil. Ce bundle doit être renseigné dans onSaveInstanceState.*/
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String valeur = savedInstanceState.getString("clé");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnQuitter = (Button) findViewById(R.id.btnQuitter);
+        btnQuitter.setOnClickListener(btnQuitterOnClickListener);
+
+        popUp("onCreate()");
+        Button btnEnvoyer = (Button) findViewById(R.id.btnEnvoyer);
+        btnEnvoyer.setOnClickListener(btnEnvoyerOnClickListener);
+
+        Button btnAct = (Button) findViewById(R.id.btnAct2);
+        btnAct.setOnClickListener(btnAct2);
+
+        if (savedInstanceState != null){
+            valeur = savedInstanceState.getString("clé");
+        }
+    }
+
+    public void setTxTValeur(String valeur) {
+        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);
+        zoneValeur.setText(valeur);
+    }
+
+    public String getTxtValeur() {
+        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);
+        return zoneValeur.getText().toString();
+    }
+
+    View.OnClickListener btnAct2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(v.getContext(), SecondActivity.class);
+            startActivity(intent);
+            intent.putExtra("clé", getTxtValeur());
+        }
+    };
+
+    View.OnClickListener btnEnvoyerOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            popUp("valeur saisie = " + getTxtValeur());
+        }
+    };
+
+    /** =============================================================
+     * Exécuté que l'activité arrêtée via un "stop" redémarre.
+     *
+     * La fonction onRestart() est suivie de la fonction onStart().
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        popUp("onRestart()");
+    }
+    /** ==============================================================
+     * Exécuté lorsque l'activité devient visible à l'utilisateur.
+     *
+     * La fonction onStart() est suivie de la fonction onResume().
+     */
+    @Override
+    protected void onStart() {
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        setTxTValeur(settings.getString("valeur", ""));
+        super.onStart();
+        popUp("onStart()");
+    }
+    /** ==============================================================
+     * Exécutée à chaque passage en premier plan de l'activité.
+     * Ou bien, si l'activité passe à nouveau en premier
+     * (si une autre activité était passée en premier plan entre temps).
+     *
+     * La fonction onResume() est suivie de l'exécution de l'activité.
+     */
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        popUp("onResume()");
+    }
+    /** =============================================================
+     * La fonction onPause() est suivie :
+     * - d'un onResume() si l'activité passe à nouveau en premier plan
+     * - d'un onStop() si elle devient invisible à l'utilisateur
+     *
+     * L'exécution de la fonction onPause() doit être rapide,
+     * car la prochaine activité ne démarrera pas tant que l'exécution
+     * de la fonction onPause() n'est pas terminée.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("valeur", getTxtValeur());
+        editor.commit();
+        if (isFinishing()) {
+            popUp("onPause, l'utilisateur à demandé la fermeture via un finish()");
+        } else {
+            popUp("onPause, l'utilisateur n'a pas demandé la fermeture via un finish()");
+        }
+    }
+    /** ==============================================================
+     * La fonction onStop() est exécutée :
+     * - lorsque l'activité n'est plus en premier plan
+     * - ou bien lorsque l'activité va être détruite
+     *
+     * Cette fonction est suivie :
+     * - de la fonction onRestart() si l'activité passe à nouveau en premier plan
+     * - de la fonction onDestroy() lorsque l'activité se termine
+     *    ou bien lorsque le système décide de l'arrêter
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        popUp("onStop()");
+    }
+    /** =============================================================
+     * Cette fonction est exécutée lorsque l'activité se termine ou bien lorsque
+     * le système décide de l'arrêter.
+     *
+     * La fonction onCreate() devra à nouveau être exécutée pour obtenir à nouveau l'activité.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        popUp("onDestroy()");
+    }
+    //=================================================================
+    View.OnClickListener btnQuitterOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+    public void popUp(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+}
